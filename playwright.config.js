9688f39d938c369a58cccccd834c2e2a96ac305a
String-Gaurav <gaurav.gitat@gmail.com>
2025-07-06 21:56:24 +0200

    changes related to multiple browser run

--- playwright.config.js
+++ playwright.config.js
@@ -5,30 +5,34 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 
-  const config = ({
-
+const config = defineConfig({
   testDir: './tests',
-  timeout: 40 *1000,
-  expect :{
-
-      timeout: 5000,
-
+  timeout: 40 * 1000,
+  expect: {
+    timeout: 5000,
   },
- reporter : 'html',
+  reporter: 'html',
   use: {
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-
-browserName : 'chromium',
-headless : true,
-screenshot : 'on',
-trace : 'retain-on-faliure'
-
-
-
+    headless: true,
+    screenshot: 'on',
+    trace: 'retain-on-failure'
   },
+  
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
 });
 
-module.exports = config
-
-
+module.exports = config;
